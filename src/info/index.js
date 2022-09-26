79f693b980be1a05cf7a38f30eef296f0fde165b
molly <molly8358158@gmail.com>
2022-09-26 17:18:26 +0800

    +GTM

--- src/info/index.js
+++ src/info/index.js
@@ -21,7 +21,7 @@ export default {
         ["建造執照", "(110)高市工建築字第02413號"],
         ["廣告代銷", ""],
     ],
-    gtmCode: ["GTM-KBWGHNV"], // 可放置多個
+    gtmCode: ["GTM-KBWGHNV","GTM-M87MK5M"], // 可放置多個
     recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC",
     // recaptcha_site_key_v2: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", //testkey
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
