c40db78752c67b6230107890dcc431346a112f86
molly <molly8358158@gmail.com>
2022-11-01 16:09:34 +0800

    增加圖片標題

--- src/section/s1.vue
+++ src/section/s1.vue
@@ -4,8 +4,8 @@
     <div class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/01.jpg" alt="" title="">
-          <img class="w-100 word word-kv" src="@/section/s1/pc/w-01.svg" data-aos="fade-up"
+          <img class="w-100" src="@/section/s1/pc/01.jpg" alt="主視覺背景" title="">
+          <img class="w-100 word word-kv" src="@/section/s1/pc/w-01.svg" alt="清景麟名人坊_標題" data-aos="fade-up"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -14,8 +14,8 @@
     <div class="container-fluid d-md-none" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_M_block">
-          <img class="w-100 mobile-kv" src="@/section/s1/m/01.jpg" alt="">
-          <img class="w-100 word word-kv" src="@/section/s1/m/w-01_1.svg" data-aos="fade-up"
+          <img class="w-100 mobile-kv" src="@/section/s1/m/01.jpg" alt="主視覺背景">
+          <img class="w-100 word word-kv" src="@/section/s1/m/w-01_1.svg" alt="清景麟名人坊_標題" data-aos="fade-up"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -24,8 +24,8 @@
     <div class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/02.jpg" alt="" title="">
-          <img class="w-100 word" src="@/section/s1/pc/w-02.svg" data-aos="fade-in"
+          <img class="w-100" src="@/section/s1/pc/02.jpg" alt="地點介紹背景" title="">
+          <img class="w-100 word" src="@/section/s1/pc/w-02.svg" alt="清景麟名人坊_地點介紹" data-aos="fade-in"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -34,8 +34,8 @@
     <div class="container-fluid d-md-none" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_M_block">
-          <img class="w-100" src="@/section/s1/m/02.jpg" alt="">
-          <img class="w-100 word" src="@/section/s1/m/w-02.svg" data-aos="fade-in"
+          <img class="w-100" src="@/section/s1/m/02.jpg" alt="地點介紹背景">
+          <img class="w-100 word" src="@/section/s1/m/w-02.svg" alt="清景麟名人坊_地點介紹" data-aos="fade-in"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -44,11 +44,11 @@
     <div class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/03_1.jpg" alt="" title="">
-          <img class="w-100 word" src="@/section/s1/pc/w-03_1.svg" alt="" title="">
-          <img class="w-100 word" src="@/section/s1/pc/w-03_2.svg" data-aos="fade-right"
+          <img class="w-100" src="@/section/s1/pc/03_1.jpg" alt="清景麟名人坊_車站圖" title="">
+          <img class="w-100 word" src="@/section/s1/pc/w-03_1.svg" alt="清景麟名人坊_生機圖" title="">
+          <img class="w-100 word" src="@/section/s1/pc/w-03_2.svg" alt="清景麟名人坊_生機介紹" data-aos="fade-right"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
-          <img class="w-100 word" src="@/section/s1/pc/w-03_3.svg">
+          <img class="w-100 word" src="@/section/s1/pc/w-03_3.svg" alt="清景麟名人坊_實景拍攝">
         </div>
       </div>
     </div>
@@ -56,8 +56,8 @@
     <div class="container-fluid d-md-none" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_M_block">
-          <img class="w-100" src="@/section/s1/m/03_1.jpg" alt="">
-          <img class="w-100 word" src="@/section/s1/m/w-03_1_1.png" data-aos="fade-right"
+          <img class="w-100" src="@/section/s1/m/03_1.jpg" alt="清景麟名人坊_車站圖">
+          <img class="w-100 word" src="@/section/s1/m/w-03_1_1.png" alt="清景麟名人坊_生機介紹" data-aos="fade-right"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -75,10 +75,10 @@
       <div class="row">
         <div class="pl-0 pr-0 C_M_block">
           <div id="pan" class="pan pl-0 pr-0 view_C_M" alt="view_C_M">
-            <img class="selected" src="@/section/s1/m/03_map.svg" alt="main">
+            <img class="selected" src="@/section/s1/m/03_map.svg" alt="清景麟名人坊_生機圖">
           </div>
           <div class="panorama_C">
-            <img src="@/section/s1/rwd/swipe_01.svg" alt="">
+            <img src="@/section/s1/rwd/swipe_01.svg" alt="手指icon">
           </div>
           <div class="">
           </div>
@@ -88,10 +88,10 @@
     <div class="container-fluid d-md-none" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_M_block">
-          <img class="w-100" src="@/section/s1/m/03_2.jpg" alt="">
-          <img class="w-100 word" src="@/section/s1/m/w-03_2.svg" data-aos="fade-right"
+          <img class="w-100" src="@/section/s1/m/03_2.jpg" alt="清景麟名人坊_岡山樂購站前廣場圖">
+          <img class="w-100 word" src="@/section/s1/m/w-03_2.svg" alt="清景麟名人坊_文字介紹" data-aos="fade-right"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
-          <img class="w-100 word" src="@/section/s1/m/w-03_3.svg" data-aos="fade-right"
+          <img class="w-100 word" src="@/section/s1/m/w-03_3.svg" alt="清景麟名人坊_圖說" data-aos="fade-right"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -100,10 +100,10 @@
     <div class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/04.jpg" alt="" title="">
-          <img class="w-100 word" src="@/section/s1/pc/w-04.svg" data-aos="fade-left"
+          <img class="w-100" src="@/section/s1/pc/04.jpg" alt="清景麟名人坊_岡山樂購站前廣場圖" title="">
+          <img class="w-100 word" src="@/section/s1/pc/w-04.svg" alt="清景麟名人坊_文字介紹" data-aos="fade-left"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
-          <img class="w-100 word" src="@/section/s1/pc/w-04-1.svg">
+          <img class="w-100 word" src="@/section/s1/pc/w-04-1.svg" alt="清景麟名人坊_圖說">
         </div>
       </div>
     </div>
@@ -111,10 +111,10 @@
     <div class="container-fluid d-md-none" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_M_block">
-          <img class="w-100" src="@/section/s1/m/04.jpg" alt="">
-          <img class="w-100 word" src="@/section/s1/m/w-04.png" data-aos="fade-up"
+          <img class="w-100" src="@/section/s1/m/04.jpg" alt="清景麟名人坊_生活圈圖片">
+          <img class="w-100 word" src="@/section/s1/m/w-04.png" alt="清景麟名人坊_文字介紹" data-aos="fade-up"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
-          <img class="w-100 word" src="@/section/s1/m/w-04-2.svg" data-aos="fade-up"
+          <img class="w-100 word" src="@/section/s1/m/w-04-2.svg" alt="清景麟名人坊_文字介紹" data-aos="fade-up"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -123,10 +123,10 @@
     <div class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/05.jpg" alt="" title="">
-          <img class="w-100 word" src="@/section/s1/pc/w-05_1.svg" data-aos="fade-up"
+          <img class="w-100" src="@/section/s1/pc/05.jpg" alt="清景麟名人坊_生活圈圖片" title="">
+          <img class="w-100 word" src="@/section/s1/pc/w-05_1.svg" alt="清景麟名人坊_文字介紹" data-aos="fade-up"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
-          <img class="w-100 word" src="@/section/s1/pc/w-05-1.svg">
+          <img class="w-100 word" src="@/section/s1/pc/w-05-1.svg" alt="清景麟名人坊_文字介紹">
         </div>
       </div>
     </div>
@@ -134,15 +134,15 @@
     <div class="container-fluid d-md-none" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_M_block">
-          <img class="w-100" src="@/section/s1/m/05_2.jpg" alt="">
+          <img class="w-100" src="@/section/s1/m/05_2.jpg" alt="清景麟名人坊_平面圖">
           <div class="A_carousel_m" style="position: absolute;top:0;right: 0;">
             <div id="A_carousel_m1" class="carousel slide" data-bs-ride="carousel">
               <div class="carousel-inner">
                 <div class="carousel-item active">
-                  <img src="@/section/s1/m/05-s1.png" class="d-block w-100" alt="...">
+                  <img src="@/section/s1/m/05-s1.png" class="d-block w-100" alt="清景麟名人坊_建築外觀">
                 </div>
                 <div class="carousel-item">
-                  <img src="@/section/s1/m/05-s2.png" class="d-block w-100" alt="...">
+                  <img src="@/section/s1/m/05-s2.png" class="d-block w-100" alt="清景麟名人坊_建築外觀">
                 </div>
               </div>
               <button class="carousel-control-next" type="button" data-bs-target="#A_carousel_m1"
@@ -152,14 +152,14 @@
               </button>
             </div>
           </div>
-          <img class="w-100 word" src="@/section/s1/m/w-05_3.svg" data-aos="fade-in"
+          <img class="w-100 word" src="@/section/s1/m/w-05_3.svg" alt="清景麟名人坊_文字介紹" data-aos="fade-in"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
-          <img class="w-100 word" src="@/section/s1/m/w-05_2.svg" data-aos="fade-in"
+          <img class="w-100 word" src="@/section/s1/m/w-05_2.svg" alt="清景麟名人坊_文字介紹" data-aos="fade-in"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">               
         </div>
         <div class="col-12 pl-0 pr-0 A_M_block">
-          <img class="w-100" src="@/section/s1/m/05_3.jpg" alt="">
-          <img class="w-100 word" src="@/section/s1/m/w-05-3.png" data-aos="fade-in"
+          <img class="w-100" src="@/section/s1/m/05_3.jpg" alt="清景麟名人坊_建材圖">
+          <img class="w-100 word" src="@/section/s1/m/w-05-3.png" alt="清景麟名人坊_文字介紹" data-aos="fade-in"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -168,15 +168,15 @@
     <div id="block-f" class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/06_1.jpg" alt="" title="">
+          <img class="w-100" src="@/section/s1/pc/06_1.jpg" alt="清景麟名人坊_平面樣品屋圖" title="">
           <div class="A_carousel_F" style="position: absolute;top:0;right: 0;width: 100%;">
             <div id="A_carousel_F" class="carousel slide" data-bs-ride="carousel">
               <div class="carousel-inner">
                 <div class="carousel-item active">
-                  <img src="@/section/s1/pc/06-s1.png" class="d-block w-100" alt="...">
+                  <img src="@/section/s1/pc/06-s1.png" class="d-block w-100" alt="清景麟名人坊_建築外觀">
                 </div>
                 <div class="carousel-item">
-                  <img src="@/section/s1/pc/06-s2.png" class="d-block w-100" alt="...">
+                  <img src="@/section/s1/pc/06-s2.png" class="d-block w-100" alt="清景麟名人坊_建築外觀">
                 </div>
               </div>
               <button class="carousel-control-next" type="button" data-bs-target="#A_carousel_F"
@@ -186,7 +186,7 @@
               </button>
             </div>
           </div>
-          <img class="w-100 word" src="@/section/s1/pc/w-06-2.svg" data-aos="fade-in"
+          <img class="w-100 word" src="@/section/s1/pc/w-06-2.svg"  alt="清景麟名人坊_文字介紹" data-aos="fade-in"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -202,16 +202,16 @@
             <div id="A_carousel_m" class="carousel slide" data-bs-ride="carousel">
               <div class="carousel-inner">
                 <div class="carousel-item active">
-                  <img src="@/section/s1/m/06-1_2.jpg" class="d-block w-100" alt="...">
-                  <img src="@/section/s1/m/w-06-1-2.png" class="d-block w-100 word" alt="...">
+                  <img src="@/section/s1/m/06-1_2.jpg" class="d-block w-100" alt="清景麟名人坊_獲獎肯定圖">
+                  <img src="@/section/s1/m/w-06-1-2.png" class="d-block w-100 word" alt="清景麟名人坊_文字介紹">
                 </div>
                 <div class="carousel-item">
-                  <img src="@/section/s1/m/06-2_2.jpg" class="d-block w-100" alt="...">
-                  <img src="@/section/s1/m/w-06-2-2.png" class="d-block w-100 word" alt="...">
+                  <img src="@/section/s1/m/06-2_2.jpg" class="d-block w-100" alt="清景麟名人坊_獲獎肯定圖">
+                  <img src="@/section/s1/m/w-06-2-2.png" class="d-block w-100 word" alt="清景麟名人坊_文字介紹">
                 </div>
                 <div class="carousel-item">
-                  <img src="@/section/s1/m/06-3_1.jpg" class="d-block w-100" alt="...">
-                  <img src="@/section/s1/m/w-06-3-3.png" class="d-block w-100 word" alt="...">
+                  <img src="@/section/s1/m/06-3_1.jpg" class="d-block w-100" alt="清景麟名人坊_獲獎肯定圖">
+                  <img src="@/section/s1/m/w-06-3-3.png" class="d-block w-100 word" alt="清景麟名人坊_文字介紹">
                 </div>
               </div>
               <button class="carousel-control-prev" type="button" data-bs-target="#A_carousel_m"
@@ -233,10 +233,10 @@
     <div class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/07_1.jpg" alt="" title="">
-          <img class="w-100 word" src="@/section/s1/pc/w-07.png" data-aos="fade-up"
+          <img class="w-100" src="@/section/s1/pc/07_1.jpg" alt="清景麟名人坊_建材照片" title="">
+          <img class="w-100 word" src="@/section/s1/pc/w-07.png" alt="清景麟名人坊_文字介紹" data-aos="fade-up"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
-          <img class="w-100 word" src="@/section/s1/pc/w-07_2.svg">
+          <img class="w-100 word" src="@/section/s1/pc/w-07_2.svg" alt="清景麟名人坊_文字介紹">
         </div>
       </div>
     </div>
@@ -244,8 +244,8 @@
     <div class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/08-1.jpg" alt="" title="">
-          <img class="w-100 word" src="@/section/s1/pc/w-08-1.svg" data-aos="fade-in"
+          <img class="w-100" src="@/section/s1/pc/08-1.jpg" alt="清景麟名人坊_獲獎肯定圖" title="">
+          <img class="w-100 word" src="@/section/s1/pc/w-08-1.svg" alt="清景麟名人坊_文字介紹" data-aos="fade-in"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -254,8 +254,8 @@
     <div class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/09.jpg" alt="" title="">
-          <img class="w-100 word" src="@/section/s1/pc/w-09_1.svg" data-aos="fade-right"
+          <img class="w-100" src="@/section/s1/pc/09.jpg" alt="清景麟名人坊_保固圖" title="">
+          <img class="w-100 word" src="@/section/s1/pc/w-09_1.svg" alt="清景麟名人坊_文字介紹" data-aos="fade-right"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
@@ -264,8 +264,8 @@
     <div class="container-fluid d-none d-md-block" style="overflow: hidden;">
       <div class="row">
         <div class="col-12 pl-0 pr-0 A_block">
-          <img class="w-100" src="@/section/s1/pc/10.jpg" alt="" title="">
-          <img class="w-100 word word-kv" src="@/section/s1/pc/w-10.svg" data-aos="fade-in"
+          <img class="w-100" src="@/section/s1/pc/10.jpg" alt="清景麟名人坊_視覺圖" title="">
+          <img class="w-100 word word-kv" src="@/section/s1/pc/w-10.svg" alt="清景麟名人坊_標題" data-aos="fade-in"
             data-aos-anchor-placement="top-center" data-aos-offset="0" data-aos-easing="ease-in-sine">
         </div>
       </div>
