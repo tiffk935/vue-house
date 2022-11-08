c8efc4882fd563800d9b7f3d25e680b7b3fefbdb
molly <molly8358158@gmail.com>
2022-11-08 10:55:14 +0800

    修改

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,6 +11,7 @@
   <!--loading end-->
   <Nav v-if="config.showNav" />
   <div class="home bg-white overflow-hidden font-['Noto_Sans_TC']">
+    <h1 style="display:none;">戀JIA</h1>
     <S1 />
     <S2 />
     <S3 />
