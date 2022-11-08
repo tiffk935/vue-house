c3ad73d377354529e1385b0f28b045a30196496b
molly <molly8358158@gmail.com>
2022-11-08 11:56:04 +0800

    增加h1

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,6 +11,7 @@
   <!--loading end-->
   <Nav v-if="config.showNav" />
   <div class="home bg-white overflow-hidden font-['Noto_Sans_TC']">
+    <h1 style="display:none;">双和心</h1>
     <S1 />
     <S2 />
     <S3 />
