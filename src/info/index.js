247a24c8b8f9766343b26568c36027d6f881d816
molly <molly8358158@gmail.com>
2022-12-05 09:35:20 +0800

    修改金鑰&後台網址

--- src/info/index.js
+++ src/info/index.js
@@ -22,7 +22,7 @@ export default {
         ["企劃銷售", "海沃創意行銷"],
     ],
     gtmCode: ["GTM-M6L9FJ9"], // 可放置多個
-    recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC",
+    recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
     // recaptcha_site_key_v2: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", //testkey
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
