572d2d2e8ba6ccc0a2df104e087beff5bb8c26b2
xcatx <xcatx796@gmail.com>
2022-12-23 10:08:06 +0800

    +客戶gtm

--- src/info/index.js
+++ src/info/index.js
@@ -19,7 +19,7 @@ export default {
         ["房型規劃", "3房,3+1房"],
         ["建築代銷", ""],
     ],
-    gtmCode: ["GTM-K9N5LN9"], // 可放置多個
+    gtmCode: ["GTM-K9N5LN9","GTM-M87MK5M"], // 可放置多個
     recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC",
     // recaptcha_site_key_v2: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", //testkey
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
