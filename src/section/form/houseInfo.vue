c276c06284a3ca304bb648672fa5b90bf5c06dd1
molly <molly8358158@gmail.com>
2023-03-15 15:41:05 +0800

    修改

--- src/section/form/houseInfo.vue
+++ src/section/form/houseInfo.vue
@@ -19,11 +19,11 @@
             </div>
         </div>
     </div>
-    <div class="footer flex items-center justify-center w-full h-[40px] bg-[#302626]">
+    <!--div class="footer flex items-center justify-center w-full h-[40px] bg-[#302626]">
         <a href="https://www.lixin.com.tw/" target="_blank"><img class="hover:opacity-50"
                 src="@/section/form/footerlogo.png" alt="德林哲里" srcset=""></a>
         <a href="https://www.h65.tw/admin/test/login" target="_blank"><p class="text-white text-xs">網頁製作</p></a>
-    </div>
+    </div-->
 </template>
 
 <style lang="scss">
@@ -66,7 +66,7 @@
 }
 
 @media screen and (max-width:768px) {
-    .footer {
+    .info-box {
         margin-bottom: size-m(63);
     }
 
