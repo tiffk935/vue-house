eb57fa5d3843c311cc41e9a1ebf4c02ec92af3bd
molly <molly8358158@gmail.com>
2023-03-17 09:31:42 +0800

    修改地址

--- src/info/index.js
+++ src/info/index.js
@@ -6,7 +6,7 @@ export default {
         description: caseName,
         keywords: caseName,
     },
-    address: "台南市東區小東路196號旁（與光明街口）",
+    address: "台南市東區小東路196號旁(與光明街交叉口)",
     googleSrc: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3672.6555357179136!2d120.22793899999999!3d22.999691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDU5JzU4LjkiTiAxMjDCsDEzJzQwLjYiRQ!5e0!3m2!1szh-TW!2stw!4v1677048418326!5m2!1szh-TW!2stw",
     googleLink: "https://goo.gl/maps/e1VLsqyeWzG6TDPn6",
     phone: "06-2083366",
@@ -19,7 +19,7 @@ export default {
         ["房型規劃", "22-32坪"],
         ["聯絡電話", "06-2083366"],
         ["代銷團隊", "悅揚國際實業有限公司"],
-        ["接待中心", "台南市東區小東路196號旁(與光明街口）"],
+        ["接待中心", "台南市東區小東路196號旁(與光明街交叉口)"],
     ],
     gtmCode: ["GTM-K2FBNVJ","GTM-M87MK5M"], // 可放置多個
     recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
