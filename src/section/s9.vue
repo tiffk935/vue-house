13fe5f3b226f32b8e96218467adaf07a47978f16
molly <molly8358158@gmail.com>
2023-03-21 14:14:10 +0800

    修改

--- src/section/s9.vue
+++ src/section/s9.vue
@@ -63,6 +63,13 @@
             <div class="text">*此為廣告效果示意，為單一建物腦3D透視表現，<br class="md:hidden">周遭環境係電腦合成，建設公司保有建物外觀修正之權利。</div>
           </div>
         </swiper-slide>
+        <swiper-slide>
+          <div class="item w-full relative">
+            <img class="w-full hidden md:block" src="@/section/s9/7.png" />
+            <img class="w-full md:hidden" src="@/section/s9/7-m.jpg" />
+            <div class="text">*此為廣告效果示意，為單一建物腦3D透視表現，<br class="md:hidden">周遭環境係電腦合成，建設公司保有建物外觀修正之權利。</div>
+          </div>
+        </swiper-slide>
       </swiper>
       <div class="wline md:hidden"></div>
 
