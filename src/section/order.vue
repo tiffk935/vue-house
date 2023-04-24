64a225697dbc79d9f8001f9776b06768e3fe354d
molly <molly8358158@gmail.com>
2023-04-24 10:43:34 +0800

    非必填

--- src/section/order.vue
+++ src/section/order.vue
@@ -243,7 +243,7 @@ const sending = ref(false)
 
 //非必填
 // const bypass = ["msg", "room_type", "email"]
-const bypass = ["msg", "email", "source_note"];
+const bypass = ["msg", "email", "source_note","source"];
 
 //中文對照
 const formDataRef = ref([
