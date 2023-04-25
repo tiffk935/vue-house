3d75f564986e01a1de0b4c139b65e3c6cda211a4
xcatx <xcatx796@gmail.com>
2023-04-25 10:01:12 +0800

    amssd1

--- src/info/index.js
+++ src/info/index.js
@@ -22,7 +22,7 @@ export default {
         ["接待中心", "台南市東區小東路196號旁(與光明街交叉口)"],
     ],
     gtmCode: ["GTM-K2FBNVJ","GTM-M87MK5M"], // 可放置多個
-    recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC",
+    recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
     // recaptcha_site_key_v2: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", //testkey
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
