d7c2f58b834aab768fa985a00aff0fa7eec95e4c
xcatx <xcatx796@gmail.com>
2023-05-02 13:51:32 +0800

    S4

--- src/section/s4.vue
+++ src/section/s4.vue
@@ -2,17 +2,14 @@
   <section class="s4 relative parallax">
     <div class="photo absolute md:hidden"></div>
 
-    <div class="content absolute text-white hidden md:block"></div>
-    <div class="t1 absolute text-white fade">推窗見綠、出門入園，<br />豈止珍稀而已。</div>
-    <div class="t2 absolute text-white fade">
+    <div class="content absolute text-white">
+    <div class="t1 text-white fade">推窗見綠、出門入園，<br />豈止珍稀而已。</div>
+    <div class="t2 text-white fade">
       面積總計逾一公頃的社子公園，為社子<br />
       內最大綠地公園，也為「德林哲里」住戶<br />
       帶來朗闊棟距與視野，公園宅奇貨可居，<br />
       綠金價值等同生活品質。
-    </div>
-    <img class="bg bg01 absolute" src="@/section/s1/01.gif" />
-    <img class="bg bg02 absolute" src="@/section/s1/02.gif" />
-
+    </div></div>
     <!--
     <div class="content-m absolute text-white md:hidden">
       推窗見綠、出門入園，<br />
@@ -81,51 +78,60 @@
   }
 
   .content {
-    top: size-m(529);
-    left: size-m(115.4);
-      width: size-m(145);
-      height:size-m(300);
+    // top: size-m(509);
+    top: size-m(450);
+    left: size-m(0);
+    width: 100%;
+     // width: size-m(145);
+    display:flex;
+    height: size-m(410);
+    justify-content:center;
+    align-items:center;
+    // opacity: 0.5;
+    flex-direction:row-reverse;
+    gap:size-m(12);
+
     @media screen and (min-width:768px) {
       top: size(0);
       left: size(1614.67);
       width: size(209.31);
       height: size(898.5);
-      background: linear-gradient(0deg, rgba(17, 161, 150, 1) -11.37%, #004B47 99.89%);
-      opacity: 0.7;
+      background: linear-gradient(0deg, rgba(17, 161, 150, .7) -11.37%, rgba(0, 75, 71, 0.7) 99.89%);
+      gap:size(36);
+    flex-direction:column;
     }
   }
-
   .t1 {
-    top: size-m(509);
-    left: size-m(210.3);
     font-size: size-m(19);
     line-height: 1.8;
     font-weight: 500;
     writing-mode: vertical-rl;
+    margin:auto 0;
+    padding: 0 0 5.6em;
+    white-space: nowrap;
     @media screen and (min-width:768px) {
       font-size: size(33);
       line-height: 1.4;
-      top: size(46.17);
-      left: size(1676.01);
+      margin:0 auto;
+    padding: 0;
     }
   }
 
   .t2 {
-    top: size-m(509);
-    left: size-m(90.4);
     font-size: size-m(15);
     line-height: 1.8;
       font-weight: 300;
     letter-spacing: .1em;
     writing-mode: vertical-rl;
     font-family: 'Noto sans TC';
+    white-space: nowrap;
+      margin:auto 0;
     @media screen and (min-width:768px) {
-      top: size(413.42);
-      left: size(1656.63);
       font-size: size(19);
       line-height: 1.7;
       font-weight: 200;
       letter-spacing: .2em;
+      margin:0 auto 1.6em auto;
     }
   }
 }
