94d4df1e4ba08377f4a02f80963d23a536444370
xcatx <xcatx796@gmail.com>
2023-07-11 16:37:27 +0800

    動畫跟壓圖

--- src/section/s1.vue
+++ src/section/s1.vue
@@ -1,22 +1,19 @@
 <template>
   <section class="s1 relative">
-    <img class="md:hidden w-full" src="@/section/s1/bg-m.jpg" />
-    <img class="hidden md:block w-full" src="@/section/s1/bg.jpg" />
-    <img class="tree1 absolute" src="@/section/s1/backtree.png" />
-    <img class="tree2 absolute" src="@/section/s1/backtree.png" />
-    <img class="tree3 absolute" src="@/section/s1/backtree.png" />
+    <img class="w-full bg" src="@/section/s1/bg.svg" >  
+    <tree class="tree3"/>
+    <tree class="tree2"/>
+    <tree class="tree1"/>
+    <tree class="tree4"/>
     <img class="car absolute" src="@/section/s1/car.png" />
-    <img class="tree4 absolute" src="@/section/s1/fronttree1.png" />
-    <img class="treepc absolute hidden md:block" src="@/section/s1/fronttree2.png" />
-    <img class="tree5 absolute" src="@/section/s1/fronttree3.png" />
-    <img class="tree6 absolute" src="@/section/s1/fronttree2.png" />
-    <img class="tree7 absolute" src="@/section/s1/fronttree3.png" />
-    <img class="tree8 absolute" src="@/section/s1/fronttree2.png" />
-    <img class="tree9 absolute" src="@/section/s1/fronttree3.png" />
-    <img class="vec1 absolute md:hidden" src="@/section/s1/vec1-m.svg" />
-    <img class="vec1 absolute hidden md:block" src="@/section/s1/vec1.svg" />
-    <img class="vec2 absolute md:hidden" src="@/section/s1/vec2-m.svg" />
-    <img class="vec2 absolute hidden md:block" src="@/section/s1/vec2.svg" />
+    <tree class="tree5"/>
+    <tree class="tree6"/>
+    <tree class="treepc hidden md:block" />
+    <tree class="tree7"/>
+    <tree class="tree8"/>
+    <tree class="tree9"/>
+    <img class="vec1 absolute" src="@/section/s1/vec1.svg" />
+    <img class="vec2 absolute" src="@/section/s1/vec1.svg" />
     <img class="logo absolute" src="@/section/s1/logo.svg" />
     <div class="rect absolute md:hidden"></div>
     <img class="t1 absolute" data-aos="fade-up" data-aos-delay="0" src="@/section/s1/t1.svg" />
@@ -33,135 +30,182 @@
 
 .s1 {
   width: 100%;
+  background: url("@/section/s1/bg.jpg");
+    background-size:size-m(450) auto;
   @media screen and (min-width:768px) {
-    
+    background-size:size(900) auto;
   }
+.bg{height:size-m(1124);max-width: unset;width: auto;margin-left: -17%;
 
-  .tree1 {
-    top: size-m(-89);
-    left: size-m(-247);
-    width: size-m(464.38);
-    max-width: none;
+  @media screen and (min-width:768px) {
+    height:auto;margin-left:0;
+    width: 100%;
+  }
+}
+.tree1{
+    top: size-m(220);
+    left: size-m(-129);
+    width: size-m(252);
     @media screen and (min-width:768px) {
-      top: size(-505);
-      left: size(-932);
-      width: size(1925);
+      top: size(790);
+      left: size(-432);
+      width: size(1030);
     }
-  }
-
-  .tree2 {
-    top: size-m(99.14);
-    left: size-m(-56);
-    width: size-m(261.92);
-    max-width: none;
+}
+.tree2{
+    top: size-m(275);
+    left: size-m(12);
+    width: size-m(140);
     @media screen and (min-width:768px) {
-      top: size(-270);
-      left: size(-217);
-      width: size(1234);
+    top: 29vw;
+    left: 3.3vw;
+    width: 34vw;
     }
-  }
-
+}
   .tree3 {
-    top: size-m(20);
-    left: size-m(52);
-    width: size-m(261.92);
-    max-width: none;
+    top: size-m(197);
+    left: size-m(120);
+    width: size-m(140);
     @media screen and (min-width:768px) {
-      top: size(121);
-      left: size(485);
-      width: size(696);
+      top: size(585);
+      left: size(660);
+      width: size(385);
     }
   }
 
   .car {
-    top: size-m(295.2);
-    left: size-m(110.22);
-    width: size-m(164.845);
-    transform: rotate(-15.062deg);
+    top: 23%;
+    left: 86%;
+    width: size-m(160);
+   // transform: rotate(-40deg);
+    transform-origin: 50% 50%;
+  //transform:  rotate(-52deg) translate(-202%, -130%) scale(-1.25, 1.25);
+    animation: car_m 3s linear infinite;
     @media screen and (min-width:768px) {
-      top: size(711.19);
-      left: size(508.84);
-      width: size(622.24);
+    top: 10%;
+    left: 86%;
+    width: size(600);
+    animation: car 3s linear infinite;
+    
     }
   }
 
+@keyframes car_m {
+  0%{
+  transform: rotate(0deg)translate(0%, 0%) scale(1);
+  }
+  30%{
+  transform:rotate(-31deg) translate(-160%, -66%) scale(1.2);
+  }
+  44%{
+    transform: rotate(-52deg) translate(-222%, -130%) scale(1.25);
+  }
+44.1%{
+  transform:rotate(52deg) translate(-12%, 290%) scale(-1.25, 1.25);
+  }
+ 100%{
+  transform: rotate(52deg) translate(448%, 330%) scale(-1.8, 1.8);
+  }
+}
+
+@keyframes car {
+  0%{
+  transform: rotate(-40deg)translate(0%, 0%) scale(1);
+  }
+  20%{
+  transform:rotate(-9deg) translate(-115%, 36%) scale(1.1);
+  }
+  30%{
+  transform: rotate(-20deg) translate(-210%, -1%) scale(1.1);
+  }
+  44%{
+  transform: rotate(-40deg) translate(-265%, -97%) scale(1.12);
+  }
+44.1%{
+  transform:rotate(50deg) translate(-25%, 353%) scale(-1.12, 1.12);
+  }
+ 100%{
+  transform:rotate(34deg) translate(287%, 448%) scale(-1.6, 1.6)
+  }
+}
   .tree4 {
-    top: size-m(-164);
-    left: size-m(77);
-    width: size-m(369);
-    max-width: none;
+    top: size-m(-143);
+    left: size-m(185);
+    width: size-m(190);
+    transform: scaleY(-1);
     @media screen and (min-width:768px) {
-      top: size(-606);
-      left: size(-534);
-      width: size(1150);
+    top: -27vw;
+    left: 45vw;
+    width: 32vw;
     }
   }
 
-  .tree5 {
-    top: size-m(-133);
-    left: size-m(106.57);
-    width: size-m(443.67);
-    max-width: none;
-    @media screen and (min-width:768px) {
-      top: size(-379);
-      left: size(713);
-      width: size(1199);
-    }
-  }
 
-  .tree6 {
-    top: size-m(-60.84);
-    left: size-m(118.78);
-    width: size-m(443.67);
-    max-width: none;
-    @media screen and (min-width:768px) {
-      top: size(-184);
-      left: size(746);
-      width: size(1199);
-    }
+.tree5 {
+  top: size-m(167);
+  left: size-m(222);
+  width: size-m(237);
+  @media screen and (min-width:768px) {
+    top: 26vw;
+    left: 54vw;
+    width: 30vw;
   }
+}
 
-  .tree7 {
-    top: size-m(830);
-    left: size-m(-304);
-    width: size-m(565);
-    max-width: none;
-    @media screen and (min-width:768px) {
-      top: size(2905);
-      left: size(-20);
-      width: size(1366);
-    }
+.tree6 {
+  top: size-m(236);
+  left: size-m(210);
+  width: size-m(240);
+    transform: scaleX(-1);
+  @media screen and (min-width:768px) {
+    top: 33vw;
+    left: 49vw;
+    width: 32vw;
   }
+}
 
-  .tree8 {
-    top: size-m(478);
-    left: size-m(206);
-    width: size-m(337.71);
-    max-width: none;
-    @media screen and (min-width:768px) {
-      top: size(1288);
-      left: size(952);
-      width: size(1150);
-    }
+
+.tree7 {
+    top: 322vw;
+    left: -42vw;
+    width: 80.3vw;
+  @media screen and (min-width:768px) {
+    top: 199.2vw;
+    left: 17.3vw;
+    width: 38.3vw;
   }
+}
 
-  .tree9 {
-    top: size-m(772);
-    left: size-m(228);
-    width: size-m(340.64);
-    max-width: none;
-    @media screen and (min-width:768px) {
-      top: size(2231);
-      left: size(1245);
-      width: size(1160);
-    }
+
+.tree8 {
+    top: 188vw;
+    left: 73.5vw;
+    width: 48vw;
+    transform: scaleX(-1);
+  @media screen and (min-width:768px) {
+    top: 107.2vw;
+    left: 61.9vw;
+    width: 32.3vw;
   }
+}
 
-  .treepc {
+.tree9 {
+    top: 267vw;
+    left: 84vw;
+    width: 48.5vw;
+  @media screen and (min-width:768px) {
+    top: 156.6vw;
+    left: 80.4vw;
+    width: 32.5vw;
+  }
+}
+
+.treepc {
     @media screen and (min-width:768px) {
-      top: size(-577);
-      left: size(960);
-      width: size(1925);
+    top: 36.7vw;
+    left: 70.5vw;
+    width: 53vw;
+    transform: scaleX(-1);
     }
   }
 
@@ -173,7 +217,7 @@
     mix-blend-mode: multiply;
     @media screen and (min-width:768px) {
       width: size(1426);
-      top: size(3);
+      top: size(-20);
       left: size(247);
     }
   }
@@ -186,7 +230,7 @@
     mix-blend-mode: multiply;
     @media screen and (min-width:768px) {
       width: size(1426);
-      top: size(321);
+      top: size(300);
       left: size(247);
     }
   }
@@ -243,4 +287,6 @@
 </style>
 
 <script setup>
+import tree from './tree.vue';
+
 </script>
---
+++ src/section/tree.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tree">
+  <img src="@/section/s1/trunk.png" class="trunk" alt="" />
+  <img src="@/section/s1/leaf.png" class="leaf" alt="" />
+</div>
+</template>
+
+<style lang="scss">
+.tree{position: absolute;
+  img{max-width: unset;}
+  .trunk{position: relative;width: 100%;}
+  .leaf{
+    
+    position: absolute;
+    width: 196%;
+    top: -107%;
+    left: -55.5%;
+    /*position: absolute;
+    width: 200%;
+    top: -109%;
+    left: -60%;*/
+    animation: leaf 3s ease-in-out infinite alternate;
+    transform-origin: 50% 85%;
+    transform: skewX(3deg);
+  }
+}
+@keyframes leaf {
+  to{
+transform:skewX(-5deg);
+  }
+}
+</style>
+<script setup>
+
+</script>
