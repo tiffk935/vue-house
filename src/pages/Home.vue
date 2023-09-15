1c809427c07736bbff4fc0d4de93f4c84b34a72c
xcatx <xcatx796@gmail.com>
2023-09-15 11:42:04 +0800

    拿掉空拍

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -13,7 +13,7 @@
   <div class="home bg-white overflow-hidden font-['Noto_Sans_TC'] bg-[#E97290]">
     <h1 style="display:none;">研森</h1>
     <S1 />
-    <S2 />
+    <!-- <S2 /> -->
     <S3 />
     <S4 />
     <S5 />
@@ -26,7 +26,7 @@
 <script setup>
 import info from "@/info"
 import S1 from "@/section/s1.vue"
-import S2 from "@/section/s2.vue"
+// import S2 from "@/section/s2.vue"
 import S3 from "@/section/s3.vue"
 import S4 from "@/section/s4.vue"
 import S5 from "@/section/s5.vue"
