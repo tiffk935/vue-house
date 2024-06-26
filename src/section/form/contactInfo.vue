99d0624b2ba11e3d2713b6f9f3da31597fb055e5
molly <molly8358158@gmail.com>
2024-06-26 09:41:20 +0800

    修改

--- src/section/form/contactInfo.vue
+++ src/section/form/contactInfo.vue
@@ -106,7 +106,7 @@
   .logo {
     width: 19.1145833333vw;
     height: 8.9583333333vw;
-    background-image: url("@/section/s1/logo.svg");
+    //background-image: url("@/section/s1/logo.svg");
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
