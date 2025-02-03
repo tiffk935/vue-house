b8cc0bf689462ec47491712701189494458e4d8b
xcatx <xcatx796@gmail.com>
2025-02-03 16:47:47 +0800

    拿掉備註必填

--- src/section/order.vue
+++ src/section/order.vue
@@ -333,7 +333,7 @@ const formData = reactive({
 })
 
 //非必填
-const bypass = ["room_type","budget", "city", "area","email","project"]
+const bypass = ["room_type","budget", "city", "area","email","project","msg"]
 
 //中文對照
 const formDataRef = ref([
