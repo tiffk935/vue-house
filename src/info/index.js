6365b266e4f77d319b06a58aff66cbcfea78d6c8
molly8351858 <molly8358158@gmail.com>
2025-03-14 14:37:29 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -10,12 +10,12 @@ export default {
   googleSrc: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3672.729505777192!2d120.23719439999999!3d22.9969722!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDU5JzQ5LjEiTiAxMjDCsDE0JzEzLjkiRQ!5e0!3m2!1szh-TW!2stw!4v1741772734208!5m2!1szh-TW!2stw",
   googleLink: "https://maps.app.goo.gl/6J1ZYtgJpWStioxH6",
   phone: "06-273-8888",
-  fbLink: "http://facebook.com/xxxxxxxxxx/",
-  fbMessage: "https://m.me/xxxxxxxxxx/",
+  fbLink: "http://facebook.com/61573695851738/",
+  fbMessage: "https://m.me/61573695851738/",
   igLink: "#",
   lineLink: "https://lin.ee/qhddC6H",
   caseName: caseName,
-  gtmCode: ["GTM-MB2MRHWF"], // 可放置多個
+  gtmCode: ["GTM-12345678"], // 可放置多個
   recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
   //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
   //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
