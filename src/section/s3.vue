b943cdf7b1180d5961f56900f99a9466abf915ef
molly8351858 <molly8358158@gmail.com>
2025-04-21 10:53:05 +0800

    修改

--- src/section/s3.vue
+++ src/section/s3.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="s3 user-n w-full relative">
     <!-- <video class="block w-full" src="https://h35.banner.tw/yjp/pc.mov" playsinline autoplay muted loop></video>  -->
-     <img src="./s3/video.jpg" alt="">
+     <div class="video-container">
+      <iframe src="https://www.youtube.com/embed/CEmR6lMm4f8?autoplay=1&mute=1&loop=1&playlist=CEmR6lMm4f8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
   </section>
 </template>
 
@@ -18,6 +20,21 @@
     left: 0;
     width: 100%;
     height: 100%;
+  }
+
+  .video-container {
+    width: 100%;
+    height: 0;
+    padding-top: 56.25%;
+    position: relative;
+
+    iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 </style>
