597d674481a2d66edada02fc414c1fb9dfac0a23
molly8351858 <molly8358158@gmail.com>
2025-04-24 11:41:36 +0800

    修改

--- src/layout/navbar.vue
+++ src/layout/navbar.vue
@@ -405,12 +405,15 @@ const scrollTo = (el) => {
 }
 
 const go = () => {
-  if (modalType.value == 'phone') {
-    window.location.href = "phoneThanks";
-  } else if (modalType.value == 'fb') {
-    window.open(info.fbMessage);
-  } else if (modalType.value == 'gmap') {
-    window.open(info.googleLink);
+  if (modalType.value == "phone") {
+    window.location.href = `tel:${info.phone.replace("-", "")}`
+     setTimeout(() => {
+       window.location.href = "phoneThanks";
+     }, 1000);
+  } else if (modalType.value == "fb") {
+    window.open(info.fbMessage)
+  } else if (modalType.value == "gmap") {
+    window.open(info.googleLink)
   }
 }
 
