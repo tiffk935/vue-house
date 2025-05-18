2cf13f989c45b49829342f3d48a84b72861685db
Tiffany Keng <tiffk935@gmail.com>
2025-05-18 17:09:06 +0800

    改gallary

--- src/pages/Gallary.vue
+++ src/pages/Gallary.vue
@@ -2,14 +2,229 @@
   <section class="banner">
     <img class="mob" src="@/assets/work/banner-m.webp" />
     <img class="pc" src="@/assets/work/banner.webp" />
-  </section>
-  <section class="content">
     <div class="title">
-      <div class="t1">港都豪宅建築的定潮者</div>
-      <div class="t2">想把最好的都帶進台灣來，這條路我們走了13年</div>
+      <div>
+        <div class="t1">走在前，讓後面跟上來</div>
+        <div class="t2">把最好的留在台灣</div>
+      </div>
     </div>
-    <img class="gallary mob" src="@/assets/work/gallary-m.webp" />
-    <img class="gallary pc" src="@/assets/work/gallary.webp" />
+  </section>
+  
+  <section class="s2">
+    <img class="t1" src="@/assets/gallary/s2/t1.svg" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0" />
+    <div class="t2" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">13年推敲琢磨，看見高雄現代建築美學的各種可能</div>
+    <div class="img1">
+      <img src="@/assets/gallary/s2/img1.jpg" />
+      <div class="img-txt">2F | 空中花園</div>
+    </div>
+    <div class="content">
+      <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+        <img class="name" src="@/assets/gallary/s2/name.svg" />
+        <div class="c1">
+          全球高端豪宅天際線定潮者<br>
+          以亞灣的寧靜之詩，寫現代建築精神
+        </div>
+        <div class="c2">
+          41層輕量化現代建築美學，<br>
+          隱藏著複雜的結構、序列、對稱等設計之美，<br>
+          透過帷幕牆和隔柵運用，打造地標型態的纖長量體，<br>
+          擴張網單元與陽台的律動，勾勒了天際與海洋，<br>
+          靜謐、極簡、詩意的美學，成就一眼難忘的住宅典範。
+        </div>
+      </div>
+      <div class="img2">
+        <img src="@/assets/gallary/s2/img2.jpg" />
+        <div class="img-txt">2F | 空中花園</div>
+      </div>
+    </div>
+  </section>
+
+  <section class="s3">
+    <div class="img1">
+      <img src="@/assets/gallary/s3/img1.jpg" />
+      <div class="img-txt">枡野俊明 | 迎賓入口</div>
+    </div>
+    <div class="content">
+      <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+        <img class="name" src="@/assets/gallary/s3/name.svg" />
+        <div class="c1">
+          日本禪宗庭園國寶巨匠<br>
+          以「德潤身」昇華【定潮】的美
+        </div>
+        <div class="c2">
+          從繁忙街廓步入，循山岩巨壁穩步前行，<br>
+          彷彿瞬間為都會降噪，如置身隱世花園之境，<br>
+          在水瀑、禪石與老樹相伴下，<br>
+          巨大的寧靜從內在升起，歸家的感動油然而生。
+        </div>
+      </div>
+      <div class="img2">
+        <img src="@/assets/gallary/s3/img2.jpg" />
+        <div class="img-txt">枡野俊明 | 入口景觀</div>
+      </div>
+    </div>
+  </section>
+
+  <section class="s4">
+    <div class="img1">
+      <img src="@/assets/gallary/s4/img1.jpg" />
+      <div class="img-txt">1F | 禪廳</div>
+    </div>
+    <div class="content">
+      <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+        <img class="name" src="@/assets/gallary/s4/name.svg" />
+        <div class="c1">
+          Aman安縵飯店御用設計師<br>
+          台灣唯一住宅作品
+        </div>
+        <div class="c2">
+          JAYA以「有根的現代設計」，尊重在地文化，從不複製。<br>
+          當JAYA開始為定潮思考設計的那一剎那，<br>
+          親自創作圖騰、家具，參與打版、設計軟裝物件…<br>
+          每個角落都是傳奇，<br>
+          每件作品，都是讓世界驚豔的原創。
+        </div>
+      </div>
+      <div class="img2">
+        <img src="@/assets/gallary/s4/img2.jpg" />
+        <div class="img-txt">1F | 大廳</div>
+      </div>
+    </div>
+  </section>
+
+  <section class="s5">
+    <div class="items">
+      <div class="item">
+        <div class="img">
+          <img src="@/assets/gallary/s5/img1.jpg" />
+          <div class="img-txt">1F | 大廳</div>
+        </div>
+        <div class="content">
+          <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+            <div class="c1">心音空響</div>
+            <div class="c2">
+              走進定潮如古剎靜謐的長廊，<br>
+              樂音透過手工銅製水缽池產生律動，<br>
+              波光粼粼的美，反射在天花板上，<br>
+              身在定潮，逐漸安坐下來，不似世俗，更似空山。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <div class="img">
+          <img src="@/assets/gallary/s5/img2.jpg" />
+          <div class="img-txt">1F | 書房．靜園</div>
+        </div>
+        <div class="content">
+          <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+            <div class="c1">所見皆美</div>
+            <div class="c2">
+              開門見深，飛泉落下，世界瞬間歸零。<br>
+              窗櫺雕艷，木棉四綻，高雄文化深根。<br>
+              <br>
+              這扇窗，連結世界的虛與實。<br>
+              這片景，映照心靈的深與邃。<br>
+              這一境，重回安縵的寧與靜。
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="s6">
+    <div class="img1">
+      <img src="@/assets/gallary/s6/img1.jpg" />
+      <div class="img-txt">2F | 全日餐廳</div>
+    </div>
+    <div class="content">
+      <div class="img2">
+        <img src="@/assets/gallary/s6/img2.jpg" />
+        <div class="img-txt">2F | 全日餐廳</div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <div class="c1">溫味日常</div>
+          <div class="c2">
+            【定潮】的全日餐廳，點的是寵愛，吃的是溫暖。<br>
+            專屬大管家「不多一分熱、不少一口茶」的細緻，<br>
+            專屬主廚以心精烹，重溫心底的《一頓燒》，<br>
+            由「吃」到「品」，從食物的味，嚐生活的道，<br>
+            好久沒有感動當下，好久沒有善待自己！
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="s7">
+    <div class="img1">
+      <img src="@/assets/gallary/s7/img1.jpg" />
+      <div class="img-txt">2F | 全日餐廳</div>
+    </div>
+    <div class="content">
+      <div class="img2">
+        <img src="@/assets/gallary/s7/img2.jpg" />
+        <div class="img-txt">40F | SKY LOUNGE</div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <div class="c1">天際悠飲</div>
+          <div class="c2">
+            【定潮】層次豐富的空間，帶來不同人生之樂，<br>
+            從寧靜到歡聚，再到一飲天地之美。<br>
+            <br>
+            第一杯：敬大隱於心！<br>
+            第二杯：敬親友齊聚！<br>
+            第三杯：酹潮月與自飲！
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="s8">
+    <div class="title">定潮 聚集了一群一心向善，胸懷寬廣之人</div>
+    <div class="items">
+      <div class="item" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+        <img src="@/assets/gallary/s8/1.jpg" />
+        <div class="c2">
+          JID團隊Thierry往返台灣超過5次，只為反覆演算、<br>
+          調整水波揚聲器的角度與光影折射。
+        </div>
+      </div>
+      <div class="item" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+        <img src="@/assets/gallary/s8/2.jpg" />
+        <div class="c2">
+          枡野俊明以德潤身，專注造景，造庭即是修行，<br>
+          庭園景觀獲得另一種深刻價值。
+        </div>
+      </div>
+      <div class="item" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+        <img src="@/assets/gallary/s8/3.jpg" />
+        <div class="c2">
+          TFB-Andrew利用消隱手法，<br>
+          讓定潮的明亮與幽微恰如其分，形成一種空間魅力。
+        </div>
+      </div>
+      <div class="item" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+        <img src="@/assets/gallary/s8/4.jpg" />
+        <div class="c2">
+          南韓傳奇廚僧-正寬法師於定潮舉辦客座餐宴，<br>
+          為高雄美食文化與國際交流寫下新篇章。  
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="s9">
+    <div class="title" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">種種不同，皆觸身心。<br>日夜有別，皆是歸心。</div>
+    <div class="img">
+      <img src="@/assets/gallary/s9/img.jpg" />
+      <div class="img-txt">1F | 街廓一景</div>
+    </div>
+    <img class="logo" src="@/assets/gallary/s9/logo.svg" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0" />
   </section>
 </template>
 
@@ -31,58 +246,441 @@
 }
 
 .banner {
-  padding: 0 0 func.size-m(27) 0;
-  @media screen and (min-width:768px) {
-    padding: 0 0 func.size(44) 0;
-  }
-
   img {
     width: 100%;
   }
-}
-
-.content {
-  padding: 0 func.size-m(30) func.size-m(86) func.size-m(30);
-  @media screen and (min-width:768px) {
-    padding: 0 func.size(160) func.size(215) func.size(160);
-  }
 
   .title {
-    margin-bottom: func.size-m(46);
-    padding-bottom: func.size-m(23);
-    border-bottom: func.size-m(1) solid #000;
+    text-align: center;
+    padding: 0 func.size-m(30);
     @media screen and (min-width:768px) {
-      display: flex;
-      justify-content: space-between;
-      align-items: flex-end;
-      margin-bottom: func.size(157);
-      padding-bottom: func.size(44);
-      border-bottom: func.size(1) solid #000;
+      text-align: left;
+      padding: 0 func.size(160);
+    }
+
+    & > div {
+      padding: func.size-m(26) 0 func.size-m(23) 0;
+      border-bottom: func.size-m(1) solid #000;
+      @media screen and (min-width:768px) {
+        padding: func.size(34) 0 func.size(44) 0;
+        border-bottom: func.size(1) solid #000;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+      }
     }
 
     .t1 {
       font-size: func.size-m(20);
+      line-height: func.size-m(24);
       font-weight: 700;
       margin-bottom: func.size-m(9);
       @media screen and (min-width:768px) {
         font-size: func.size(78);
-        margin-bottom: 0;
         line-height: 1;
+        margin-bottom: 0;
       }
     }
 
     .t2 {
       font-size: func.size-m(12);
+      line-height: func.size-m(14);
       @media screen and (min-width:768px) {
         font-size: func.size(32);
-        margin-bottom: 0;
         line-height: 1;
+        letter-spacing: .05em;
+      }
+    }
+  }
+}
+
+.c1 {
+  font-family: "Noto Serif TC";
+  font-size: func.size-m(15);
+  font-weight: 700;
+  line-height: 133.2%;
+  margin-bottom: func.size-m(13);
+  @media screen and (min-width:768px) {
+    font-size: func.size(44);
+    line-height: 146.2%;
+    margin-bottom: func.size(93);
+  }
+}
+
+.c2 {
+  font-size: func.size-m(12);
+  line-height: func.size-m(20);
+  margin-bottom: func.size-m(13);
+  @media screen and (min-width:768px) {
+    font-size: func.size(20);
+    line-height: func.size(36);
+    margin-bottom: 0;
+  }
+}
+
+.s2, .s3, .s4 {
+  padding: func.size-m(43) 0 func.size-m(21) 0;
+  @media screen and (min-width:768px) {
+    padding: func.size(102) func.size(169) func.size(133) func.size(169);
+  }
+
+  .img1, .img2 {
+    position: relative;
+
+    img {
+      width: 100%;
+      display: block;
+    }
+
+    .img-txt {
+      position: absolute;
+      bottom: func.size-m(8);
+      right: func.size-m(8);
+      color: #FFF;
+      font-family: "Noto Serif TC";
+      font-size: func.size-m(10);
+      font-weight: 700;
+      line-height: 133.2%;
+      @media screen and (min-width:768px) {
+        bottom: func.size(11);
+        right: func.size(18);
+        font-size: func.size(15);
       }
     }
   }
 
-  .gallary {
-    width: 100%;
+  .img1 {
+    margin-bottom: func.size-m(29);
+    @media screen and (min-width:768px) {
+      margin-bottom: func.size(233);
+    }
+  }
+
+  .img2 {
+    @media screen and (min-width:768px) {
+      width: func.size(777);
+    }
+  }
+
+  .content {
+    padding: 0 func.size-m(30);
+    @media screen and (min-width:768px) {
+      padding: 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-end;
+    }
+  }
+
+  .name {
+    width: auto;
+    height: func.size-m(36);
+    display: block;
+    margin-bottom: func.size-m(19);
+    @media screen and (min-width:768px) {
+      height: func.size(115);
+      margin-bottom: func.size(45.5);
+    }
+  }
+}
+
+.s2 {
+  .t1 {
+    display: block;
+    width: func.size-m(260);
+    margin: 0 auto func.size-m(7) auto;
+    @media screen and (min-width:768px) {
+      width: func.size(794.17);
+      margin: 0 auto func.size(21) auto;
+    }
+  }
+
+  .t2 {
+    text-align: center;
+    font-family: "Noto Serif TC";
+    font-size: func.size-m(14);
+    font-weight: 700;
+    line-height: 133.2%;
+    margin-bottom: func.size-m(39);
+    @media screen and (min-width:768px) {
+      font-size: func.size(44);
+      margin-bottom: func.size(107);
+    }
+  }
+}
+
+.s3 {
+  .content {
+    @media screen and (min-width:768px) {
+      flex-direction: row-reverse;
+    }
+  }
+
+  .name {
+    margin-right: 0;
+    margin-left: auto;
+  }
+
+  .c1, .c2 {
+    text-align: right;
+  }
+}
+
+.s5 {
+  padding: func.size-m(48) func.size-m(30) func.size-m(76) func.size-m(30);
+  @media screen and (min-width:768px) {
+    padding: func.size(60) func.size(169);
+  }
+
+  .img {
+    position: relative;
+    @media screen and (min-width:768px) {
+      width: func.size(814);
+    }
+
+    img {
+      width: 100%;
+      display: block;
+    }
+
+    .img-txt {
+      position: absolute;
+      bottom: func.size-m(5);
+      right: func.size-m(7);
+      color: #FFF;
+      font-family: "Noto Serif TC";
+      font-size: func.size-m(10);
+      font-weight: 700;
+      line-height: 133.2%;
+      @media screen and (min-width:768px) {
+        bottom: func.size(10);
+        right: func.size(17);
+        font-size: func.size(15);
+      }
+    }
+  }
+
+  .items {
+    .item {
+      background: #fff;
+      margin-bottom: func.size-m(22);
+      @media screen and (min-width:768px) {
+        display: flex;
+        align-items: stretch;
+        margin-bottom: func.size(164);
+      }
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  .content {
+    padding: func.size-m(27) func.size-m(18);
+    @media screen and (min-width:768px) {
+      padding: func.size(29) func.size(67);
+      width: calc(100% - func.size(814));
+      display: flex;
+      align-items: flex-end;
+    }
+  }
+
+  .c2 {
+    margin: 0;
+  }
+}
+
+.s6, .s7 {
+  padding: func.size-m(42) 0 func.size-m(48) 0;
+  @media screen and (min-width:768px) {
+    padding: func.size(102) func.size(169);
+  }
+
+  .img1, .img2 {
+    position: relative;
+
+    img {
+      width: 100%;
+      display: block;
+    }
+
+    .img-txt {
+      position: absolute;
+      bottom: func.size-m(8);
+      right: func.size-m(8);
+      color: #FFF;
+      font-family: "Noto Serif TC";
+      font-size: func.size-m(10);
+      font-weight: 700;
+      line-height: 133.2%;
+      @media screen and (min-width:768px) {
+        bottom: func.size(11);
+        right: func.size(18);
+        font-size: func.size(15);
+      }
+    }
+  }
+
+  .img1 {
+    margin-bottom: func.size-m(41);
+    @media screen and (min-width:768px) {
+      margin-bottom: func.size(24);
+    }
+  }
+
+  .img2 {
+    @media screen and (min-width:768px) {
+      width: func.size(884);
+    }
+  }
+
+  .content {
+    padding: 0 func.size-m(30);
+    @media screen and (min-width:768px) {
+      padding: 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: stretch;
+    }
+
+    & > div:nth-child(2) {
+      background: #fff;
+      padding: func.size-m(25) func.size-m(18);
+      @media screen and (min-width:768px) {
+        padding: func.size(26) func.size(38);
+        width: calc(100% - func.size(884 + 24));
+        display: flex;
+        align-items: flex-end;
+      }
+    }
+  }
+}
+
+.s6 .content {
+  @media screen and (min-width:768px) {
+    flex-direction: row-reverse;
+  }
+}
+
+.s8 {
+  padding: func.size-m(40) func.size-m(30);
+  @media screen and (min-width:768px) {
+    padding: func.size(120) 0;
+  }
+
+  .title {
+    text-align: center;
+    font-family: "Noto Serif TC";
+    font-size: func.size-m(14);
+    font-weight: 700;
+    line-height: 133.2%;
+    margin-bottom: func.size-m(36);
+    @media screen and (min-width:768px) {
+      font-size: func.size(44);
+      margin-bottom: func.size(183);
+    }
+  }
+
+  .items {
+    @media screen and (min-width:768px) {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+    }
+
+    .item {
+      margin-bottom: func.size-m(21);
+      @media screen and (min-width:768px) {
+        width: func.size(537);
+        margin: 0 func.size(133.5) func.size(133) func.size(133.5);
+      }
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+
+      &:nth-last-child(1), &:nth-last-child(2) {
+        @media screen and (min-width:768px) {
+          margin-bottom: 0;
+        }
+      }
+
+      img {
+        width: 100%;
+        display: block;
+        margin-bottom: func.size-m(10);
+        @media screen and (min-width:768px) {
+          margin-bottom: func.size(22);
+        }
+      }
+
+      .c2 {
+        padding: 0 func.size-m(18);
+        @media screen and (min-width:768px) {
+          padding: 0;
+        }
+      }
+    }
+  }
+}
+
+.s9 {
+  padding: func.size-m(12) 0 func.size-m(95) 0;
+  @media screen and (min-width:768px) {
+    padding: func.size(17) 0 func.size(227) 0;
+  }
+
+  .title {
+    text-align: center;
+    font-family: "Noto Serif TC";
+    font-size: func.size-m(14);
+    font-weight: 700;
+    line-height: 133.2%;
+    margin-bottom: func.size-m(56);
+    @media screen and (min-width:768px) {
+      font-size: func.size(44);
+      margin-bottom: func.size(155);
+    }
+  }
+
+  .img {
+    position: relative;
+    margin-bottom: func.size-m(82);
+    @media screen and (min-width:768px) {
+      width: func.size(1341);
+      margin: 0 auto func.size(213) auto;
+    }
+
+    img {
+      width: 100%;
+      display: block;
+    }
+
+    .img-txt {
+      position: absolute;
+      bottom: func.size-m(8);
+      right: func.size-m(8);
+      color: #FFF;
+      font-family: "Noto Serif TC";
+      font-size: func.size-m(10);
+      font-weight: 700;
+      line-height: 133.2%;
+      @media screen and (min-width:768px) {
+        bottom: func.size(11);
+        right: func.size(18);
+        font-size: func.size(15);
+      }
+    }
+  }
+
+  .logo {
+    display: block;
+    width: func.size-m(90);
+    margin: 0 auto;
+    @media screen and (min-width:768px) {
+      width: func.size(155);
+    }
   }
 }
 </style>
