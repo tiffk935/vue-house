669a1f8374d376333fd7e08fd922ab0fb90012fd
molly8351858 <molly8358158@gmail.com>
2025-06-20 14:24:13 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -2,7 +2,7 @@ const caseName = "紅布朗花園"
 
 export default {
     caseid: "22739067-8e85-4064-aebd-cf1c16379926",
-    case_code: "hbl",
+    //case_code: "hbl",
     address: "接待中心:新北市土城區明德路一段343號旁",
     googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7233.139757395482!2d121.45165569561678!3d24.98074463657931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3468033c05ba0839%3A0xa3ff3cfafc480798!2z57SF5biD5pyX6Iqx5ZyS!5e0!3m2!1szh-TW!2stw!4v1749104158140!5m2!1szh-TW!2stw",
     googleLink: "https://maps.app.goo.gl/4cS1pFZMfQjiNq849",
