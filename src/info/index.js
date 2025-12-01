a95734b420348ac357e02256ca3469329a8ecf1e
xcatx <xcatx796@gmail.com>
2025-12-01 15:31:04 +0800

    gtm

--- src/info/index.js
+++ src/info/index.js
@@ -18,7 +18,7 @@ export default {
         ["基地面積", "XXXX"],
         ["格局規劃", "XXXX"],
     ],
-    gtmCode: ["GTM-TN23QFFJ"], // 可放置多個
+    gtmCode: ["GTM-569RX26M"], // 可放置多個
     recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
