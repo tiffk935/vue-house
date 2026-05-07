cada37d4e961cec39f0c94cec96b78026f8f4f0a
Tiffany Keng <tiffk935@gmail.com>
2026-05-07 16:06:55 +0800

    加 CURLOPT_REFERER

--- contact-form.php
+++ contact-form.php
@@ -50,6 +50,7 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_REFERER, 'https://dali-heyun.tw');
 
 $response = curl_exec($ch);
 curl_close($ch);
