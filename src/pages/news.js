38f33772d2545833ee0587bf9048908e3a5dbf89
xcatx <xcatx796@gmail.com>
2026-06-22 17:57:02 +0800

    0

--- src/pages/news.js
+++ src/pages/news.js
@@ -15,18 +15,17 @@ const newsItems = document.querySelectorAll('#news-sec .news-item');
 newsItems.forEach(item => {
   item.addEventListener('click', (e) => {
     const el = e.target.closest('.news-item');
-    
     const link = el.getAttribute('data-link') || '';
     
     if (link) {
       const filter = posts.filter(post => post.link === link);
       if (filter.length > 0) {
         const selectedPost = filter[0];
-
         selectedPost.thumbnail = selectedPost.thumbnail.replace('@/assets', '../assets');
         updateModalContent(selectedPost);
 
         gsap.set('#new-modal', { display: 'block' });
+        document.body.classList.add('modal-open');
         ps = new PerfectScrollbar(document.querySelector('#new-modal .content'));
       }
     }
@@ -34,7 +33,7 @@ newsItems.forEach(item => {
 });
 
 const updateModalContent = (post) => {
-  document.querySelector('#new-modal h3').innerText = post.title;
+  document.querySelector('#new-modal h3').innerHTML = post.title;
   document.querySelector('#new-modal time').innerText = post.date;
   document.querySelector('#new-modal .author').innerText = post.author;
   document.querySelector('#new-modal figure.pc img').src = newsImages[post.thumbnail];
@@ -50,12 +49,14 @@ if (document.querySelector('#news-sec .main')) {
     updateModalContent(mainNews);
           
     gsap.set('#new-modal', { display: 'block' });
+    document.body.classList.add('modal-open');
     ps = new PerfectScrollbar(document.querySelector('#new-modal .content'));
   });
 }
 
 document.querySelector('#new-modal .close').addEventListener('click', () => {
   gsap.set('#new-modal', { display: 'none' });
+  document.body.classList.remove('modal-open');
   document.querySelector('#new-modal figure.pc img').src = '';
   document.querySelector('#new-modal figure.mob img').src = '';
   if (ps) {
