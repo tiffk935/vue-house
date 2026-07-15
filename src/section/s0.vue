43c05711b310dabc2d1e0d9aeddab3c64d1a3752
reck-png <reck@lixin.com.tw>
2026-07-15 17:24:11 +0800

    改戰鬥陀螺-完成

--- src/section/s0.vue
+++ src/section/s0.vue
@@ -1,23 +1,10 @@
 <template>
   <article class="s0" id="s0">
-    <!-- <img src="./s1/mo.jpg" class="t0"> -->
-    <div class="train absolute">
-      <img src="./s1/train.svg" alt="train" class="trainbg" v-if="!$isMobile()">
-      <img src="./s1/trainm.svg" alt="train" class="trainbg" v-else>
-      <img src="./s1/train.png" alt="train" class="train1 absolute">
-    </div>
-
-
-    <img src="./s1/logo.svg" alt="logo" class="logo" data-aos="zoom-in" data-aos-delay="0">
+    <img class="s0-main" src="@/section/s0/B-2.jpg" />
+    <img class="ball-1" src="@/section/balls/green.png" />
+    <img class="ball-2" src="@/section/balls/yellow.png" />
+    <img class="ball-3" src="@/section/balls/blue.png" />
     
-    <div class="ribbon" data-aos="zoom-in" data-aos-delay="0">
-    <img src="./s1/2.png" alt="ribbon">
-  </div>
-    <div class="bubble" data-aos="zoom-in" data-aos-delay="0">
-      <img src="./s1/bubble.svg" alt="bubble" v-if="!$isMobile()">
-      <img src="./s1/bubblem.svg" alt="bubble" v-else>
-    </div>
-    <img src="./s1/l.png" alt="logo" class="l">
 
   </article>
 </template>
@@ -25,21 +12,35 @@
 <style lang="scss" scoped>
 @import '@/assets/style/function.scss';
 
-@keyframes op {
+@keyframes 0 {
   50% {
     opacity: 0;
   }
 }
 @keyframes an {
+  from {
+    opacity: 0;
+    transform: scale(0.7);
+  }
+
   to {
-    transform: translate(0);
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
+@keyframes ball {
+  from {
+    transform: translateY(0);
+  }
+
+  to {
+    transform: translateY(-30px);
   }
 }
 
 .s0 {
-  @apply relative w-full h-screen;
-  min-height: size(900);
-  max-height: size(1070);
+  @apply relative w-full;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -57,157 +58,69 @@
     opacity: .3;
   }
 
-  .logo {
-    position: relative;
-    width: size(705);
-    margin-bottom: size(75);
-  }
-
-  .ribbon {
+  .ball-1 {
+    display: none; // 手機版隱藏
     position: absolute;
-    top: size(195);
-    left: size(-175);
-    width: size(986);
-    img {
-      width: 100%;
-      transform: rotate(-37deg);
+
+    @media (min-width: 768px) {
+      display: block; // 平板、桌機顯示
+
+      top: size(800);
+      left: size(1500);
+      width: size(475);
+      transform: translateY(size(-160));
+      align-self: flex-end;
+      animation: ball 3.5s ease-in-out 0s infinite alternate;
     }
-}
-  .bubble {
+  }
+
+  .ball-2 {
+    display: none; // 手機版隱藏
     position: absolute;
-    bottom: size(308);
-    right: size(250);
-    width: size(350);
-    img {
-      width: 100%;
-      animation: an 2s infinite ease-in-out alternate;
-      transform: rotate(-5deg);
-      transform-origin: 0% 100%;
-    }
-  }
-
-
-  @keyframes train {
-    0% {
-      transform: translate(-50vw);
-    }
-
-    55% {
-      transform: translate(60vw);
-    }
-
-    65% {
-      transform: translate(60vw);
-    }
-
-    100% {
-      transform: translate(250vw);
-    }
-  }
-
-  .train {
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    text-align: right;
-    border-bottom: size(22) solid #462E86;
-
-    .trainbg {
-      width: size(1650);
-      margin: auto auto auto 0;
-    }
-
-    .train1 {
-      bottom: size(-5);
-      right: 100%;
-      height: size(91);
-      animation: train 10s infinite;
-      max-width: none;
-    }
-  }
-  
-
-  .l {
-    position: absolute;
-    bottom: size(-80);
-    left: size(0);
-    width: size(2000);}
-}
-
-
-/* 螢幕尺寸標準 */
-/* 平板尺寸 */
-@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {}
-
-@media screen and (max-width: 767px) {
-
-
-
-  .s0 {
-    height: calc(100vh - 63px);
-    min-height: size-m(604);
-    max-height: size-m(750);
-    font-size: size-m(15);
     
-  .logo {
-    width: size-m(304);
-    margin-bottom: size-m(50);
-  }
-  .ribbon {
-    top: size-m(80);
-    left: size-m(-130);
-    width: size-m(390);
-    img {
-      transform: rotate(-37deg);
+    @media (min-width: 768px) {
+      display: block; // 平板、桌機顯示
+
+      top: size(1000);
+      left: size(1400);
+      width: size(200);
+      transform: translateY(size(-160));
+      align-self: flex-end;
+      animation: ball 3.5s ease-in-out 0s infinite alternate-reverse;
     }
+  }
+
+  .ball-3 {
+  display: none; // 手機版隱藏
+  position: absolute;
+
+  @media (min-width: 768px) {
+    display: block; // 平板、桌機顯示
+
+    top: size(80);
+    left: size(80);
+    width: size(200);
+    transform: translateY(size(-800));
+    align-self: flex-end;
+    animation: ball 3.5s ease-in-out 0s infinite alternate;
+  }
 }
-  .bubble {
-    bottom: size-m(45);
-    bottom: calc(50% + #{size-m(45 - 604 * .5)});
-    right: size-m(15);
-    width: size-m(160);
-    img {
-      transform-origin: 0% 0%;
-    }
+
+  .s0-main {
+   position: relative;
+
+  /* 手機版 */
+  width: 100%;
+  margin-top: size(0);
+  object-fit: contain;
+
+  @media (min-width: 768px) {
+    width: size(1920);
+    max-width: 48%;
+    margin-top: size(40);
+    animation: an 0.7s ease-out;
   }
-
-
-
-    @keyframes train {
-      0% {
-        transform: translate(-50vw);
-      }
-
-      55% {
-        transform: translate(35%);
-      }
-
-      65% {
-        transform: translate(35%);
-      }
-      100%{
-        transform: translate(calc(100% + 150vw));
-      }
-
-    }
-
-    .train {
-      .trainbg {
-        width: 100%;
-      }
-
-      .train1 {
-        bottom: size-m(2);
-        height: size-m(40);
-        transform: translate(100%);
-      }
-    }
-  .l {    
-    bottom: size-m(-30);
-    left: size-m(-20);
-    width: size-m(600);max-width:900px;}
-  }
-
+}
 
 }
 </style>
