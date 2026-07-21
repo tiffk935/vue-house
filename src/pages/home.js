b8c5050f162b980c490a629ebe33b3a5425d845f
Tiffany Keng <tiffk935@gmail.com>
2026-07-21 21:07:39 +0800

    首頁加loading&調整checkbox位移

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,17 @@
+import gsap from 'gsap';
+
 window.addEventListener('load', () => {
   import('@/pages/news.js');
   import('@/pages/order.js');
+
+  gsap.to('#loading', {
+    opacity: 0,
+    direction: 0.3,
+    delay: 1,
+    onComplete: () => {
+      gsap.set('#loading', {
+        display: 'none'
+      });
+    }
+  })
 });
