6e34b3204b7c51f4cd4065e80da5305a848a8ec7
Tiffany Keng <tiffk935@gmail.com>
2026-08-06 17:29:26 +0800

    調整loading畫面

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,61 @@
 import gsap from 'gsap';
 
-window.addEventListener('load', () => {
-  import('@/pages/news.js');
+let totalCount = 0;
+let completedCount = 0;
+const resourceUrls = [];
 
-  gsap.to('#loading', {
-    opacity: 0,
-    direction: 0.3,
-    delay: 1,
-    onComplete: () => {
-      gsap.set('#loading', {
-        display: 'none'
-      });
-    }
-  })
+function updateProgress() {
+  completedCount++;
+  const percent = Math.round((completedCount / totalCount) * 100);
+  
+  document.querySelector('#pct').innerText = percent + '%';
+  gsap.to('#bar', { width: `${percent}%`, ease: 'none', duration: 0.2 });
+
+  if (completedCount === totalCount) {
+    finishLoading();
+  }
+}
+
+function finishLoading() {
+  document.querySelector('#pct').innerText = '100%';
+  gsap.to('#bar', { width: '100%', ease: 'none', duration: 0.2 });
+
+  setTimeout(() => {
+    gsap.to('#loading', {
+      opacity: 0,
+      direction: 0.3,
+      delay: 1,
+      onComplete: () => {
+        gsap.set('#loading', {
+          display: 'none'
+        });
+      }
+    })
+  }, 400);
+}
+
+totalCount = document.querySelectorAll('main img').length + document.querySelectorAll('main video').length;
+
+document.querySelectorAll('main img').forEach(img => {
+  const image = new Image();
+
+  image.onload = () => {
+    updateProgress();
+  };
+
+  image.onerror = () => {
+    updateProgress();
+  };
+
+  image.src = img.src;
+});
+
+document.querySelectorAll('main video').forEach(video => {
+  video.addEventListener('loadeddata', () => {
+    updateProgress();
+  });
+
+  video.addEventListener("error", () => {
+    updateProgress();
+  });
 });
