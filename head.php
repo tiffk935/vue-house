003f6a520b501ca6de49d1bbd43ef034a959b358
reck-png <reck@lixin.com.tw>
2026-08-11 18:30:17 +0800

    改head

--- head.php
+++ head.php
@@ -1,34 +1,38 @@
 <?php
-#下3段式抓 為案件編號 $case_code
-#$case_code_test 是用來判斷是否為1的測試頁
-#$case_code = "jw";特殊案使用
-$src =$_SERVER['SERVER_NAME']; 
-$case_code_test = substr(substr($src,0,strpos($src,'.')),-1);
-$case_code = "jia";
+$data = [
+    'src' => 'https://full-in-love.tw',
+    'title' => '泰和茂營造團隊．田中新案．即將登場',
+    'description' => '泰和茂田中新案：田中站前正核心，高效雙鐵動能，市心豐美生活機能，全世代精工景觀地標，敬請期待，電話：04-829-1133',
+    'keyword' => '泰和茂營造團隊,田中建案,彰化買房,彰化高鐵建案,彰化雙鐵宅'
+];
 
-$pdo = new pdo('mysql:host=localhost;dbname=htw_web', 'htw', '748aSgl5Ni');
-$pdo->exec("SET NAMES 'utf8'");
-$sql = "SELECT title,description,keyword,p0 FROM susers WHERE email = '" . $case_code . "'";
-$dataList = $pdo->query($sql)->fetchAll();
-$docTitle = $dataList[0]['title'];
-$siteName = $dataList[0]['title'];
-$docDesc = $dataList[0]['description'];
-$keyWords = $dataList[0]['keyword'];
-if ($dataList[0]['p0']) {
-$p0 = "<meta name='facebook-domain-verification' content='". $dataList[0]['p0'] ."' />";
-}
+$docTitle = $data['title'];
+$siteName = $data['title'];
+$docDesc = $data['description'];
+$keyWords = $data['keyword'];
 $ogType = 'website';
+$shareUrl = $data['src'];
 ?>
+
 <title><?php echo $docTitle; ?></title>
 <meta name="description" content="<?php echo $docDesc ?>">
 <meta name="keywords" content="<?php echo $keyWords ?>">
+<link rel="canonical" href="<?php echo $shareUrl; ?>">
+
+<!-- Open Graph -->
 <meta property="og:locale" content="zh_TW" />
 <meta property="og:type" content="<?php echo $ogType ?>" />
 <meta property="og:title" content="<?php echo $docTitle; ?>" />
 <meta property="og:description" content="<?php echo $docDesc ?>" />
 <meta property="og:site_name" content="<?php echo $siteName; ?>" />
-<meta name="twitter:description" content="<?php echo $docDesc ?>" />
+<meta property="og:url" content="<?php echo $shareUrl; ?>" />
+
+<!-- Twitter -->
+<meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:title" content="<?php echo $docTitle; ?>" />
+<meta name="twitter:description" content="<?php echo $docDesc ?>" />
+
+<!-- Schema.org -->
 <meta itemprop="name" content="<?php echo $docTitle; ?>">
 <meta itemprop="description" content="<?php echo $docDesc ?>">
-<?php echo $p0 ?>
+
