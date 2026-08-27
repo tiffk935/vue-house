cb8c741591b0c4f8a10ec03ee9bab1307f56ae05
reck-png <reck@lixin.com.tw>
2026-08-27 17:32:32 +0800

    改發信系統2

--- head.php
+++ head.php
@@ -1,24 +1,20 @@
 <?php
-#下3段式抓 為案件編號 $case_code
-#$case_code_test 是用來判斷是否為1的測試頁
-#$case_code = "jw";特殊案使用
-$src =$_SERVER['SERVER_NAME']; 
-$case_code_test = substr(substr($src,0,strpos($src,'.')),-1);
-$case_code = "sj";
+# 2025/5/19
+#直接定義靜態資料
+$data = [
+    'src' => 'https://shih-jhu.tw/',
+    'title' => '小城故事8｜鄰梅花湖60-67坪電梯別墅社區｜官網',
+    'description' => '小城故事8：宜蘭首選，電梯別墅。有天有地高質感社區管理，雙主臥4套房，私有開心農場，自住渡假置產。預約專線：0900-799-888',
+    'keyword' => '小城故事8,宜蘭建案,宜蘭預售屋,羅東買房,電梯別墅'
+];
 
-$pdo = new pdo('mysql:host=localhost;dbname=htw_web', 'htw', '748aSgl5Ni');
-$pdo->exec("SET NAMES 'utf8'");
-$sql = "SELECT title,description,keyword,p0 FROM susers WHERE email = '" . $case_code . "'";
-$dataList = $pdo->query($sql)->fetchAll();
-$docTitle = $dataList[0]['title'];
-$siteName = $dataList[0]['title'];
-$docDesc = $dataList[0]['description'];
-$keyWords = $dataList[0]['keyword'];
-if ($dataList[0]['p0']) {
-$p0 = "<meta name='facebook-domain-verification' content='". $dataList[0]['p0'] ."' />";
-}
+$docTitle = $data['title'];
+$siteName = $data['title'];
+$docDesc = $data['description'];
+$keyWords = $data['keyword'];
 $ogType = 'website';
 ?>
+
 <title><?php echo $docTitle; ?></title>
 <meta name="description" content="<?php echo $docDesc ?>">
 <meta name="keywords" content="<?php echo $keyWords ?>">
@@ -27,8 +23,8 @@ $ogType = 'website';
 <meta property="og:title" content="<?php echo $docTitle; ?>" />
 <meta property="og:description" content="<?php echo $docDesc ?>" />
 <meta property="og:site_name" content="<?php echo $siteName; ?>" />
+<meta property="og:image" content="<?php echo $src; ?>og.jpg" />
 <meta name="twitter:description" content="<?php echo $docDesc ?>" />
 <meta name="twitter:title" content="<?php echo $docTitle; ?>" />
 <meta itemprop="name" content="<?php echo $docTitle; ?>">
 <meta itemprop="description" content="<?php echo $docDesc ?>">
-<?php echo $p0 ?>
